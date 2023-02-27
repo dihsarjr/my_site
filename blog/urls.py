c615dbaf2e9938519de378d756c57f2344ac2ee8
mohammedrashid <mohammed1rashid786@gmail.com>
2023-02-27 22:22:44 +0530

    run

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.starting_page,"staring-page"),
-    path("posts",views.posts,"posts-page"),
-    path("posts/<slug:slug>",views.post_details,"post-details-page")
+    path("",views.starting_page,name="staring-page"),
+    path("posts",views.posts,name="posts-page"),
+    path("posts/<slug:slug>",views.post_details,name="post-details-page")
 ]
